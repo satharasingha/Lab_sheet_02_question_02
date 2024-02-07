a618f73d42d6e92b9d22eae71e543ea7166f9fcf
Veru Satharasingha <Veru Satharasingha@DESKTOP-BN34K29>
2024-02-07 14:19:08 +0530

    Add project files.

---
+++ Lab_sheet_02_question_02/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Lab_sheet_02_question_02
+{
+    class BankAccount
+    {
+        public int AccountNumber { get; set; }
+        public decimal Balance { get; set; }
+
+        public void Deposit(decimal amount)
+        {
+            Balance += amount;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BankAccount myaccount = new BankAccount();
+            myaccount.AccountNumber = 00000000;
+            myaccount.Balance = 1000;
+
+            Console.WriteLine("Initial balance: $" + myaccount.Balance);
+
+            decimal depositAmount = 500;
+            myaccount.Deposit(depositAmount);
+
+            Console.WriteLine("Updated balance after deposit: $" + myaccount.Balance);
+        }
+    }
+}
